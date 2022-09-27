7a67b6fc53d57b8f0222ae01c5e4725f3caac666
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2022-09-27 13:09:35 +0530

    iamge round fix

--- src/components/TestimonialNew.js
+++ src/components/TestimonialNew.js
@@ -128,7 +128,7 @@ function TestimonialNew() {
                 </div>
               </div>
               <div className="flex flex-col items-center justify-center mt-10">
-                <img src={user} alt="profile pictre" className="w-12 h-12" />
+                <img src={user} alt="profile pictre" className="w-12 h-12 rounded-full" />
                 <p className="text-base font-semibold leading-4 text-center font-Helvetica ">
                   P Akila
                 </p>
@@ -253,7 +253,7 @@ function TestimonialNew() {
                 </div>
               </div>
               <div className="flex flex-col items-center justify-center mt-10">
-                <img src={user} alt="profile pictre" className="w-12 h-12" />
+                <img src={user} alt="profile pictre" className="w-12 h-12 rounded-full" />
                 <p className="text-base font-semibold leading-4 text-center font-Helvetica ">
                   Kaviya
                 </p>
@@ -378,7 +378,7 @@ function TestimonialNew() {
                 </div>
               </div>
               <div className="flex flex-col items-center justify-center mt-10">
-                <img src={user} alt="profile pictre" className="w-12 h-12" />
+                <img src={user} alt="profile pictre" className="w-12 h-12 rounded-full" />
                 <p className="text-base font-semibold leading-4 text-center font-Helvetica ">
                   Sujitha Rajendran
                 </p>
